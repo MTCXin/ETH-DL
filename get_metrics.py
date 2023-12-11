04faddab6d3ce8c6ff7a9a8a628d2f8772dacafa
Ruizhe Zhu <95543710+nerchio@users.noreply.github.com>
2023-12-11 01:43:26 +0100

    Add files via upload

---
+++ get_metrics.py
@@ -0,0 +1,37 @@
+# CREATED BY RUIZHE ZHU, 10/12/2023
+
+import os
+import json
+from metrics import *
+
+METRICS = [test0, test1]
+PATH = './imgs/'
+JSON_NAME = 'TEST'
+
+def feature_extract(img_path):
+    metric_dic = {}
+    for metric in METRICS:
+        metric_dic[metric.__name__] = metric(img_path)
+    return metric_dic
+
+def json_add(img_dataset, img_class, img_path, metric_dict, json_array):
+    json_dic = {}
+    json_dic['DATASET'] = img_dataset
+    json_dic['PATH'] = img_path
+    json_dic['CLASS'] = img_class
+    json_dic['METRICS'] = metric_dict
+    json_array.append(json_dic)
+
+def main():
+    json_array = []
+    for root, ds, fs in os.walk(PATH):
+        for file in fs:
+            img_dataset, img_class = root.replace(PATH, '').split('\\')
+            img_path = os.path.join(root, file)
+            metric_dic = feature_extract(img_path)
+            json_add(img_dataset, img_class, img_path, metric_dic, json_array)
+    with open(JSON_NAME+'.json', 'w', encoding='utf-8') as json_file:
+        json.dump(json_array, json_file, indent=4)
+   
+if __name__ == '__main__':
+    main()
---
+++ metrics.py
@@ -0,0 +1,5 @@
+def test0(img_path):
+    return 0
+
+def test1(img_path):
+    return 1
