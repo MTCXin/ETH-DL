89ab072bed2a30390d4da9afafaca7637a612eac
Ruizhe Zhu <95543710+nerchio@users.noreply.github.com>
2023-12-22 13:50:31 +0100

    Update get_metrics copy.py
    syntax

--- get_metrics copy.py
+++ get_metrics copy.py
@@ -4,11 +4,11 @@ import os
 import json
 from metrics import *
 
-METRICS = [Color_Histogram, Color_Moment, Dominant_Color_Descriptor, Gray_Level_Cooccurrence_Matrix, \
-            Local_Binary_Patterns, Gabor_Filters, Histogram_of_Oriented_Gradients, Sobel, Prewitt, \
-            Canny, Laplacian_of_Gaussian_Filter, Entropy, Fractal_Dimension, Edge_Density, \
-            Spatial_Information, Discrete_Fourier_Transform, Wavelet_Transform, Histogram_Equalization, \
-            Discrete_Cosine_Transform, SVD, PCA_transform]
+METRICS = [Color_Histogram, Color_Moment, Dominant_Color_Descriptor, Gray_Level_Cooccurrence_Matrix, 
+           Local_Binary_Patterns, Gabor_Filters, Histogram_of_Oriented_Gradients, Sobel, Prewitt, 
+           Canny, Laplacian_of_Gaussian_Filter, Entropy, Fractal_Dimension, Edge_Density, 
+           Spatial_Information, Discrete_Fourier_Transform, Wavelet_Transform, Histogram_Equalization,
+           Discrete_Cosine_Transform, SVD, PCA_transform]
 
 PATH = './imgs/'
 JSON_NAME = 'TEST'
